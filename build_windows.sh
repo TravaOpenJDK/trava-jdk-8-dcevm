54c630a1b5fc4bd45c84f71ae27a4e635e8feeb8
skybber <lada.dvorak7@gmail.com>
2019-01-01 15:08:33 +0100

    Fixes

--- build_windows.sh
+++ build_windows.sh
@@ -1,16 +1,21 @@
 export PATH=/cygdrive/c/tools/cygwin/bin:$PATH
 
 wget -q -O OpenJDK8_x64_Windows.zip "https://api.adoptopenjdk.net/v2/binary/releases/openjdk8?openjdk_impl=hotspot&os=windows&arch=x64&release=latest&type=jdk"
+wget -q -O freetype.zip https://github.com/ubawurinna/freetype-windows-binaries/releases/download/v2.9.1/freetype-2.9.1.zip
 
 JDK_BOOT_DIR="$PWD/$(unzip -Z1 OpenJDK8_x64_Windows.zip | grep 'bin/javac'  | tr '/' '\n' | tail -3 | head -1)"
 unzip -q OpenJDK8_x64_Windows.zip
+mkdir -p openjdk-build/freetype
+mv freetype.zip openjdk-build/freetype/
+$(cd openjdk-build/freetype/; unzip -q freetype.zip)
 
 # unset cygwin's gcc preconfigured
 unset -v CC
 unset -v CXX
 
-FREETYPE_DIR="$PWD/openjdk-build/workspace/build/installedfreetype"
+FREETYPE_DIR="$PWD/openjdk-build/freetype"
 
 cd ./openjdk-build
 export LOG=info
-./makejdk-any-platform.sh --tag "${SOURCE_JDK_TAG}" --jdk-boot-dir "${JDK_BOOT_DIR}" --configure-args "--with-freetype=${FREETYPE_DIR}" --target-file-name java8-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk8u
+export kate TOOLCHAIN_VERSION="2013"
+./makejdk-any-platform.sh --tag "${SOURCE_JDK_TAG}" --build-variant dcevm  --branch dcevm11 --jdk-boot-dir "${JDK_BOOT_DIR}" --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --configure-args "--with-freetype-include=${FREETYPE_DIR}/include --with-freetype-lib=${FREETYPE_DIR}/win64 --disable-ccache --with-extra-cflags=/W0 --with-extra-cxxflags=/W0" --target-file-name java8-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk8u

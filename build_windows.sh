2e7bd47e466ce5368f66a210219e716a780471cb
skybber <lada.dvorak7@gmail.com>
2019-01-09 19:05:01 +0100

    Fixes

--- build_windows.sh
+++ build_windows.sh
@@ -18,6 +18,4 @@ FREETYPE_DIR="$PWD/openjdk-build/freetype"
 cd ./openjdk-build
 export LOG=info
 export kate TOOLCHAIN_VERSION="2013"
-# unset VS120COMNTOOLS
-# unset VS110COMNTOOLS
-./makejdk-any-platform.sh --branch "${SOURCE_JDK_BRANCH}" --tag "${SOURCE_JDK_TAG}" --jdk-boot-dir "${JDK_BOOT_DIR}" --build-variant dcevm --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --configure-args "--with-freetype-include=${FREETYPE_DIR}/include --with-freetype-lib=${FREETYPE_DIR}/win64 --disable-ccache" --target-file-name java8-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk8u
+./makejdk-any-platform.sh --branch "${SOURCE_JDK_BRANCH}" --tag "${SOURCE_JDK_TAG}" --jdk-boot-dir "${JDK_BOOT_DIR}" --build-variant dcevm --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --configure-args "--with-freetype-include=${FREETYPE_DIR}/include --with-freetype-lib=${FREETYPE_DIR}/win64 --disable-ccache --with-extra-cflags=/W0 --with-extra-cxxflags=/W0" --target-file-name java8-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk8u

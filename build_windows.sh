9ee19167f5594d4fd303b32adbf0d284670ad3c8
Vladimir Dvorak <lada.dvorak7@gmail.com>
2019-11-10 10:10:05 +0100

    Fix OSX build

--- build_windows.sh
+++ build_windows.sh
@@ -20,4 +20,7 @@ unset VS110COMNTOOLS
 
 cd ./openjdk-build
 export LOG=info
-./makejdk-any-platform.sh --branch "${SOURCE_JDK_BRANCH}" --tag "${SOURCE_JDK_TAG}" --jdk-boot-dir "${JDK_BOOT_DIR}" --build-variant dcevm --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --configure-args "--with-freetype-include=${FREETYPE_DIR}/include --with-freetype-lib=${FREETYPE_DIR}/win64 --disable-ccache" --target-file-name java8-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk8u
+export LANG=C
+export JAVA_HOME=$JDK_BOOT_DIR
+
+./makejdk-any-platform.sh --branch "${SOURCE_JDK_BRANCH}" --tag "${SOURCE_JDK_TAG}" --jdk-boot-dir "${JDK_BOOT_DIR}" --build-variant dcevm --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --check-fingerprint false --configure-args "--with-freetype-include=${FREETYPE_DIR}/include --with-freetype-lib=${FREETYPE_DIR}/win64 --disable-ccache" --target-file-name java8-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk8u
